4ee24f166627cdabac08b6d52970d035f1c53b46
haih <k7268wre>
2018-12-26 08:42:28 +0200

    back to uniform cross over

--- gnas/search_space/cross_over.py
+++ gnas/search_space/cross_over.py
@@ -3,6 +3,6 @@ from gnas.genetic_algorithm.cross_over import uniform_crossover, select_crossove
 
 
 def individual_uniform_crossover(individual_a: Individual, individual_b: Individual) -> Individual:
-    res = [select_crossover(a, b) for a, b in zip(individual_a.iv, individual_b.iv)]
+    res = [uniform_crossover(a, b) for a, b in zip(individual_a.iv, individual_b.iv)]
 
     return Individual(res, individual_a.mi, individual_a.ss)
