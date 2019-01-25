48b63b588f9ee03c3613e52cadedf0b8c02eba97
haih <k7268wre>
2019-01-25 18:08:49 +0200

    add run config

---
+++ configs/study_n_block_two/run_block_check.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+nc run -C GPU9 python /data/projects/swat/users/haih/GNAS/main.py --config_file /data/projects/swat/users/haih/GNAS/configs/study_n_block_two/config_p_1_l_1.json
+nc run -C GPU9 python /data/projects/swat/users/haih/GNAS/main.py --config_file /data/projects/swat/users/haih/GNAS/configs/study_n_block_two/config_p_1_l_2.json
+nc run -C GPU9 python /data/projects/swat/users/haih/GNAS/main.py --config_file /data/projects/swat/users/haih/GNAS/configs/study_n_block_two/config_p_1_l_3.json
+nc run -C GPU9 python /data/projects/swat/users/haih/GNAS/main.py --config_file /data/projects/swat/users/haih/GNAS/configs/study_n_block_two/config_p_2_l_1.json
+nc run -C GPU9 python /data/projects/swat/users/haih/GNAS/main.py --config_file /data/projects/swat/users/haih/GNAS/configs/study_n_block_two/config_p_2_l_2.json
+nc run -C GPU9 python /data/projects/swat/users/haih/GNAS/main.py --config_file /data/projects/swat/users/haih/GNAS/configs/study_n_block_two/config_p_2_l_3.json
+nc run -C GPU9 python /data/projects/swat/users/haih/GNAS/main.py --config_file /data/projects/swat/users/haih/GNAS/configs/study_n_block_two/config_p_5_l_2.json
+nc run -C GPU9 python /data/projects/swat/users/haih/GNAS/main.py --config_file /data/projects/swat/users/haih/GNAS/configs/study_n_block_two/config_p_5_l_1.json
